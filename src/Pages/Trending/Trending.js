93901bc1e07d95729b89a8c2fac62812a396f793
Suvadeep <maityamalendu18@gmail.com>
2021-10-10 23:32:43 +0530

    added trending page and css part also

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,9 +1,39 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
 
 const Trending = () => {
+  const [content, setContent] = useState([]);
+
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      ` https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+
+    // console.log(data.results);
+    setContent(data.results);
+  };
+  useEffect(() => {
+    fetchTrending();
+  }, []);
+
   return (
     <div>
-      <span className="trending">trending</span>
+      <span className="pageTitle">trending</span>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={c.media_type}
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
     </div>
   );
 };
--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect } from "react";
 import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
@@ -7,7 +8,7 @@ import WhatshotIcon from "@mui/icons-material/Whatshot";
 import MovieIcon from "@mui/icons-material/Movie";
 import TvIcon from "@mui/icons-material/Tv";
 import SearchIcon from "@mui/icons-material/Search";
-// import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 const useStyles = makeStyles({
   root: {
     width: 500,
@@ -19,9 +20,24 @@ const useStyles = makeStyles({
 });
 
 export default function SimpleBottomNavigation() {
+  const classes = useStyles();
 
+  const [value, setValue] = React.useState(0);
 
-const [value, setValue] = React.useState(0);
+  const history = useHistory();
+
+  React.useEffect(() => {
+    if (value === 0) {
+      history.push("/");
+    } else if (value === 1) {
+      history.push("/movies");
+    } else if (value === 2) {
+      history.push("/series");
+    } else if (value === 3) {
+      history.push("/search");
+    }
+  }, [value,history]);
+
   return (
     <Box sx={{ width: 500 }}>
       <BottomNavigation
@@ -30,6 +46,8 @@ const [value, setValue] = React.useState(0);
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
+        showLabels
+        className={classes.root}
       >
         <BottomNavigationAction
           styles={{ color: "white" }}
